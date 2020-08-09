a9f335ec34df490799d240203a1795b6f46c6a61
Varun Raj <varunraj82@gmail.com>
2020-08-08 22:08:14 -0400

    initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+import jsonPlaceholder from '../api/jsonPlaceholder';
+
+
+
+export const fetchPosts = async () => {
+
+    const response = await jsonPlaceholder.get('/posts');
+
+    return {
+        type:'FETCH_POSTS',
+        payload: response
+    };
+};
